3415311fca8ed3bdeac9999258a963e333008ff4
Marc Becker <maarc1@gmx.de>
2022-10-11 22:05:04 +0200

    5.3 working

--- my-first-io.js
+++ my-first-io.js
@@ -1,6 +1,6 @@
 "use strict";
 const fs = require("fs");
 
-const fileContent = fs.readFileSync(process.argvh[2], "utf8");
-const numberOfLines = fileContent.split("\n").length - 1;
+const fileContent = fs.readFileSync(process.argv[2]);
+const numberOfLines = fileContent.toString().split("\n").length - 1;
 console.log(numberOfLines);
